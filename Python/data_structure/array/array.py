7c10356b3fb329291b285c730e211555ab7ba215
Ayush Jha <96854429+Ayushjha132@users.noreply.github.com>
2024-09-12 23:59:45 +0530

    array in python

--- Python/data_structure/array/array.py
+++ Python/data_structure/array/array.py
@@ -5,3 +5,44 @@ list  --> []
 tuple --> []
 
 """
+
+
+# using list
+#
+
+
+class MyArray:
+    def __init__(self):
+        self.array = []
+        self.arrlen = 0
+
+    def push(self, num):
+        self.array.append(num)
+        self.arrlen = self.arrlen + 1
+        return self.arrlen
+
+    def pop(self):
+        if self.arrlen == 0:
+            return "Array is empty"
+        last_item = self.array[self.arrlen - 1]
+        del self.array[self.arrlen - 1]
+        self.arrlen = self.arrlen - 1
+        return last_item
+
+    def length(self):
+        return self.arrlen
+
+    def showArray(self):
+        print(self.array, "\nlength: ", self.arrlen)
+
+
+arr = MyArray()
+
+arr = MyArray()
+arr.push(10)
+arr.push(20)
+arr.showArray()
+print(arr.pop())
+arr.showArray()
+print(arr.pop())
+print(arr.pop())
