11faf7c27b25d2c9deffc17ab03fb31889000b5a
Ayush Jha <96854429+Ayushjha132@users.noreply.github.com>
2024-09-13 15:40:23 +0530

    selection sort

--- Javascript/algorithms/sorting/selectionSort.js
+++ Javascript/algorithms/sorting/selectionSort.js
@@ -7,20 +7,26 @@
  * Space Complexity : O(1)
  *
  * Working : Moving elemnet by selecting the samllest one moveing to beginning of the array and repeating the value.
+ * Implemantation: Selecting one elemnet as min or max and keep checking that with other if new min/max come up swap with that. 
  *
  */
 
 
 arr = [99, 44, 6, 2, 5, 9, 1, 0, 67];
 
-function seclectionSort(arr) {
+function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let min = i;
     let temp = arr[i];
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[min] > arr[j]) {
-
+        min = j
       }
     }
+    arr[i] = arr[min]
+    arr[min] = temp;
   }
-} 
+  return arr;
+}
+
+console.log(selectionSort(arr))
